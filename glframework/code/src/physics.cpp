50566f3e270c3d7465df545a3952ed51e7bf16f5
Samuel Balcells <sFaith3@users.noreply.github.com>
2018-02-14 18:18:38 +0100

    glframework

---
+++ glframework/code/src/physics.cpp
@@ -0,0 +1,38 @@
+#include <imgui\imgui.h>
+#include <imgui\imgui_impl_sdl_gl3.h>
+
+bool show_test_window = false;
+void GUI() {
+	bool show = true;
+	ImGui::Begin("Physics Parameters", &show, 0);
+
+	// Do your GUI code here....
+	{	
+		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);//FrameRate
+		
+	}
+	// .........................
+	
+	ImGui::End();
+
+	// Example code -- ImGui test window. Most of the sample code is in ImGui::ShowTestWindow()
+	if(show_test_window) {
+		ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiSetCond_FirstUseEver);
+		ImGui::ShowTestWindow(&show_test_window);
+	}
+}
+
+void PhysicsInit() {
+	// Do your initialization code here...
+	// ...................................
+}
+
+void PhysicsUpdate(float dt) {
+	// Do your update code here...
+	// ...........................
+}
+
+void PhysicsCleanup() {
+	// Do your cleanup code here...
+	// ............................
+}
---
+++ glframework/code/src/render_prims.cpp
@@ -0,0 +1,79 @@
+#include <GL\glew.h>
+#include <glm\gtc\type_ptr.hpp>
+#include <glm\gtc\matrix_transform.hpp>
+
+// Boolean variables allow to show/hide the primitives
+bool renderSphere = false;
+bool renderCapsule = false;
+bool renderParticles = false;
+bool renderCloth = false;
+bool renderCube = false;
+
+namespace Sphere {
+	extern void setupSphere(glm::vec3 pos = glm::vec3(0.f, 1.f, 0.f), float radius = 1.f);
+	extern void cleanupSphere();
+	extern void updateSphere(glm::vec3 pos, float radius = 1.f);
+	extern void drawSphere();
+}
+namespace Capsule {
+	extern void setupCapsule(glm::vec3 posA = glm::vec3(-3.f, 2.f, -2.f), glm::vec3 posB = glm::vec3(-4.f, 2.f, 2.f), float radius = 1.f);
+	extern void cleanupCapsule();
+	extern void updateCapsule(glm::vec3 posA, glm::vec3 posB, float radius = 1.f);
+	extern void drawCapsule();
+}
+namespace LilSpheres {
+	extern const int maxParticles;
+	extern void setupParticles(int numTotalParticles, float radius = 0.05f);
+	extern void cleanupParticles();
+	extern void updateParticles(int startIdx, int count, float* array_data);
+	extern void drawParticles(int startIdx, int count);
+}
+namespace ClothMesh {
+	extern void setupClothMesh();
+	extern void cleanupClothMesh();
+	extern void updateClothMesh(float* array_data);
+	extern void drawClothMesh();
+}
+
+namespace Cube {
+	extern void setupCube();
+	extern void cleanupCube();
+	extern void updateCube(const glm::mat4& transform);
+	extern void drawCube();
+}
+
+void setupPrims() {
+	Sphere::setupSphere();
+	Capsule::setupCapsule();
+	LilSpheres::setupParticles(LilSpheres::maxParticles);
+	ClothMesh::setupClothMesh();
+	Cube::setupCube();
+}
+void cleanupPrims() {
+	Sphere::cleanupSphere();
+	Capsule::cleanupCapsule();
+	LilSpheres::cleanupParticles();
+	ClothMesh::cleanupClothMesh();
+	Cube::cleanupCube();
+}
+
+void renderPrims() {
+	if (renderSphere)
+		Sphere::drawSphere();
+	if (renderCapsule)
+		Capsule::drawCapsule();
+
+	if (renderParticles) {
+		// You may need to rethink this piece of code...
+		int startDrawingFromParticle = 0;
+		int numParticlesToDraw = LilSpheres::maxParticles;
+		LilSpheres::drawParticles(startDrawingFromParticle, numParticlesToDraw);
+		// .............................................
+	}
+	
+	if (renderCloth)
+		ClothMesh::drawClothMesh();
+
+	if (renderCube)
+		Cube::drawCube();
+}
