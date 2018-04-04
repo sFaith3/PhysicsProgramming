7971fbc1692c8e8c77e7454560fdd2138fb80813
Samuel Balcells <sFaith3@users.noreply.github.com>
2018-04-04 13:33:18 +0200

    Cube Falling

--- RigidObject/code/src/physics.cpp
+++ RigidObject/code/src/physics.cpp
@@ -1,6 +1,7 @@
 #include <imgui\imgui.h>
 #include <imgui\imgui_impl_sdl_gl3.h>
 #include <glm\glm.hpp>
+#include <vector>
 
 namespace Cube {
 	extern void updateCube(const glm::mat4& transform);
@@ -8,12 +9,18 @@ namespace Cube {
 
 //
 
+const float MASS = 1.f;
+const glm::vec3 GRAVITY = MASS * glm::vec3(0.f, -9.81f, 0.f);
+const glm::mat3 MAT_Ibody_INVERSE = glm::inverse({
+	(1.f / 12.f) * MASS * 2.f, 0.f, 0.f,
+	0.f, (1.f / 12.f) * MASS * 2.f, 0.f,
+	0.f, 0.f, (1.f / 12.f) * MASS * 2.f
+});
+
 glm::vec3 posCube;
-glm::vec3 rotCube;
+glm::mat3 rotCube;
 glm::vec3 linearMomentumCube;
 glm::vec3 angMomentumCube;
-
-glm::mat4 transMatrix;
 
 bool show_test_window = false;
 void GUI() {
@@ -38,22 +45,45 @@ void GUI() {
 
 void PhysicsInit() {
 	posCube = glm::vec3(0.f, 3.f, 0.f);
-	rotCube = glm::vec3(0.f);
+	rotCube = glm::mat3(1.f);
 	linearMomentumCube = glm::vec3(0.f);
 	angMomentumCube = glm::vec3(0.f);
-
-	transMatrix = glm::mat4(0.f);
 }
 
-void PhysicsUpdate(float dt) {
-	transMatrix = {
-		1.f, 0.f, 0.f, 0.f,
-		0.f, 1.f, 0.f, 0.f,
-		0.f, 0.f, 1.f, 0.f,
+void UpdateCube(float dt) {
+	glm::vec3 F = GRAVITY;
+	// P(t + dt) = P(t) + dt * F(t)
+	linearMomentumCube += dt * F;
+
+	glm::vec3 r = posCube + glm::vec3(1.f, 0.f, 0.f);
+	// T(t) = sum(ri(t) - x(t)) x Fi(t)
+	glm::vec3 T = (r - posCube) * F;
+	// L(t + dt) = L(t) + dt * T(t)
+	angMomentumCube += dt * T;
+
+	// v(t + dt) = P(t + dt) / M
+	glm::vec3 v = linearMomentumCube / MASS;
+
+	// x(t + dt) = x(t) + dt * v(t + dt);
+	posCube += dt * v;
+
+	// I(t)^-1 = R(t) * I^-1 * R(t)^T
+	glm::mat3 I = rotCube * MAT_Ibody_INVERSE * glm::transpose(rotCube);
+
+	//...
+
+	glm::mat4 transMatrix = {
+		rotCube[0][0], rotCube[0][1], rotCube[0][2], 0.f,
+		rotCube[1][0], rotCube[1][1], rotCube[1][2], 0.f,
+		rotCube[2][0], rotCube[2][1], rotCube[2][2], 0.f,
 		posCube.x, posCube.y, posCube.z, 1.f
 	};
 
 	Cube::updateCube(transMatrix);
+}
+
+void PhysicsUpdate(float dt) {
+	UpdateCube(dt);
 }
 
 void PhysicsCleanup() {
